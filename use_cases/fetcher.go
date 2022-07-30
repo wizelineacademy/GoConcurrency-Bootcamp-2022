29b2bde7c8e399af74f32d80bf30d5dc2b51dae5
guillermo <gromo@merqueo.com>
2022-07-29 22:27:29 -0500

    feat: implements concurrency in provide services

--- use_cases/fetcher.go
+++ use_cases/fetcher.go
@@ -1,9 +1,10 @@
 package use_cases
 
 import (
-	"strings"
-
 	"GoConcurrency-Bootcamp-2022/models"
+	"fmt"
+	"strings"
+	"sync"
 )
 
 type api interface {
@@ -25,20 +26,53 @@ func NewFetcher(api api, storage writer) Fetcher {
 
 func (f Fetcher) Fetch(from, to int) error {
 	var pokemons []models.Pokemon
-	for id := from; id <= to; id++ {
-		pokemon, err := f.api.FetchPokemon(id)
-		if err != nil {
-			return err
-		}
+	var errors []error
+	var wg = sync.WaitGroup{}
 
-		var flatAbilities []string
-		for _, t := range pokemon.Abilities {
-			flatAbilities = append(flatAbilities, t.Ability.URL)
-		}
-		pokemon.FlatAbilityURLs = strings.Join(flatAbilities, "|")
+	numWorkers := 10
 
-		pokemons = append(pokemons, pokemon)
+	// generate channel by nums id
+	chGenerator := make(chan int)
+	go f.generatorChannel(from, to, chGenerator)
+
+	// create 10 workers
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func(chGenerator chan int) {
+			defer wg.Done()
+			for ch := range chGenerator {
+				pokemon, err := f.api.FetchPokemon(ch)
+
+				if err != nil {
+					errors = append(errors, fmt.Errorf("error get id %d, %w", ch, err))
+					continue
+				}
+
+				var flatAbilities []string
+				for _, t := range pokemon.Abilities {
+					flatAbilities = append(flatAbilities, t.Ability.URL)
+				}
+				pokemon.FlatAbilityURLs = strings.Join(flatAbilities, "|")
+
+				pokemons = append(pokemons, pokemon)
+
+			}
+		}(chGenerator)
 	}
 
+	wg.Wait()
+
+	// print errors
+	fmt.Println("errors count", len(errors))
+
 	return f.storage.Write(pokemons)
+}
+
+// generatorChannel generate multiple channel
+func (f Fetcher) generatorChannel(from, to int, chGenerator chan int) {
+	for id := from; id <= to; id++ {
+		chGenerator <- id
+	}
+
+	close(chGenerator)
 }
